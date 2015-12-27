18221fee77a37c753b610a7357befc6314945218
Daiki Murata <pmw1415.rkmbl@gmail.com>
2015-12-27 12:41:58 +0900

    Mdy: SettingFragment: add setting item "icon type"

--- app/src/main/java/jp/or/pmw1415/mascotonnotification/SettingFragment.java
+++ app/src/main/java/jp/or/pmw1415/mascotonnotification/SettingFragment.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
@@ -18,8 +19,11 @@ public class SettingFragment extends PreferenceFragment
 	private Context mContext;
 	// key
 	private String mKeyNotificationEnabled;
+	private String mKeyIconType;
 
 	private boolean mBeforeSettingEnabled;
+
+	private String[] mListPrefIconType;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -28,12 +32,24 @@ public class SettingFragment extends PreferenceFragment
 
 		mContext = this.getActivity();
 		mKeyNotificationEnabled = this.getString((R.string.notification_enabled_key));
+		mKeyIconType = this.getString(R.string.icon_type_key);
 
 		CheckBoxPreference prefEnabled = (CheckBoxPreference)findPreference(mKeyNotificationEnabled);
 		prefEnabled.setOnPreferenceChangeListener(this);
 
 		SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(mContext);
 		mBeforeSettingEnabled = sharedPref.getBoolean(mKeyNotificationEnabled, false);
+
+		mListPrefIconType = this.getResources().getStringArray(R.array.icon_type);
+		ListPreference prefIconType = (ListPreference)findPreference(this.getString(R.string.icon_type_key));
+		prefIconType.setOnPreferenceChangeListener(this);
+
+		String iconType = sharedPref.getString(mKeyIconType, mListPrefIconType[0]);
+		if (prefIconType.getValue() == null) {
+			prefIconType.setValue(iconType);
+		}
+		prefIconType.setSummary(iconType);
+
 	}
 
 	@Override
@@ -53,6 +69,11 @@ public class SettingFragment extends PreferenceFragment
 
 	@Override
 	public boolean onPreferenceChange(Preference preference, Object newValue) {
+		if (preference.getKey().equals(mKeyIconType)) {
+			ListPreference prefIconType = (ListPreference)preference;
+			prefIconType.setSummary((String)newValue);
+		}
+
 		return true;
 	}
 
