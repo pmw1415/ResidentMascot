07f3cae15757e210cb096e63f0a0bc82adfa6364
Daiki Murata <pmw1415.rkmbl@gmail.com>
2015-12-28 04:06:42 +0900

    Mdy: MyBroadcastReceiver: add action
    - ACTION_TIME_TICK
    - ACTION_SCREEN_ON

--- app/src/main/java/jp/or/pmw1415/mascotonnotification/MyBroadcastReceiver.java
+++ app/src/main/java/jp/or/pmw1415/mascotonnotification/MyBroadcastReceiver.java
@@ -3,6 +3,8 @@ package jp.or.pmw1415.mascotonnotification;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.util.Log;
 
 /**
  * Created by pmw1415 on 2015/12/27.
@@ -12,11 +14,37 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		String action = intent.getAction();
-		if (action.equals(Intent.ACTION_BOOT_COMPLETED)) {
+		Log.d("MyBroadcastReceiver", "action = " + action);
+		if (action.equals(Intent.ACTION_BOOT_COMPLETED) ||
+				action.equals(Intent.ACTION_SCREEN_ON) ||
+				action.equals(Intent.ACTION_TIME_TICK)) {
 			Intent intentService = new Intent();
 			intentService.setClassName(context.getPackageName(), NotificationAnimationService.class.getName());
 			context.startService(intentService);
 		}
 
+	}
+
+	/**
+	 * レシーバ登録
+	 *
+	 * manifestで登録できないアクションを登録する。
+	 *
+	 * @param context
+	 */
+	public void register(Context context) {
+		IntentFilter filter = new IntentFilter();
+		filter.addAction(Intent.ACTION_TIME_TICK);
+		filter.addAction(Intent.ACTION_SCREEN_ON);
+		context.getApplicationContext().registerReceiver(this, filter);
+	}
+
+	/**
+	 * レシーバ登録解除
+	 *
+	 * @param context
+	 */
+	public void unregister(Context context) {
+		context.getApplicationContext().unregisterReceiver(this);
 	}
 }
